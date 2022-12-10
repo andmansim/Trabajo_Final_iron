23c811bca302eef3ed01d25207adb64292833127
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-10 12:29:58 +0100

    explicaciones

--- proyecto.py
+++ proyecto.py
@@ -13,5 +13,8 @@ df.info()
 
 #Observación: columnas no numércicas: date, bathrooms(arreglar), floors(arreglar), lat(arreglar), long(arreglar)
 # En principio no se ve ningún dato nulo  
+#Como queremos predecir el precio, la columna que va a ir por separado es price
+#Para predecir el precio necesitamos: bedrooms, bathrooms, sqft (los examinamos para ver si los podemos agrupar), floors, condiciones, grade, year_built, year_renovate, waterview
+#lat, long no nos influye ya que estan todas las casas en la misma zona de Seattle, Tacoma y alrededores (hemos buscado en google maps)
 
 #Separamos variables numéricas y categóricas 
