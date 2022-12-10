8801c98ce2eb8bb3d6d4a19f5b8aee3a303c11a6
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-10 22:01:59 +0100

    fkngxdjklfhgjklfglfkgjfjgkfjgkfjgkdfjñgdlño

--- proyecto.py
+++ proyecto.py
@@ -41,11 +41,11 @@ def redondear(columna):
             df[columna][i] = round(df[columna][i])
     return df
 
-redondear('bathrooms')
+'''redondear('bathrooms')
 print(df['bathrooms'])
 redondear('floors')
 print(df['floors'])
-
+'''
 #Ahora vamos a quitar las columnas que no nos hacen falta: lat, long, date, zipcode
 
 df = df.drop(columns = ['date', 'lat', 'long', 'zipcode'], axis = 1)
@@ -70,16 +70,95 @@ df = df.drop(columns = ['yr_renovated'], axis = 1)
 print(df['yr_built'])
 
 #Las columnas que tengan que ver con numero de habitaciones, las podemos agrupar para simpllificar los calculos
-df['habitaciones'] = 0
+'''df['habitaciones'] = 0
 for i in range(len(df['bedrooms'])):
-       df['habitaciones'][i] = int(df['bedrooms'][i]) + int(df['bathrooms'][i])
+       df['habitaciones'][i] = int(df['bedrooms'][i]) + int(df['bathrooms'][i])'''
 
 #Ahora que el dataset está limpio, vamos a pasar a hacer la regresión
 #Primero, vamos a entrenar y testear.
 
-'''print(df.corr())
+print(df.corr())
 
-y = df['price']
+#Por la matriz de correlacion vemos que los unicos datos con los que el precio puede estar más relacionado es con los 
+#metros cadrados de vivienda y con la calidad (sqft_living15, grade)
+#Por tanto, vamos a probar la recta de regresión con esas dos columnas
+
+x2 = df['price']
+
+y6 = df['grade']
+y7 = df['sqft_living15']
+
+x2_const = sm.add_constant(x2)
+
+def regresion(y, x, xconst):  
+    modelo = sm.OLS(y, xconst).fit()
+    pred = modelo.predict(xconst)
+    try:
+        const = modelo.params[0]
+        coef = modelo.params[1]
+        x_l = np.linspace(x.min(), x.max(), 50)
+        y_l = coef*x_l + const
+    except:
+        pass
+
+    plt.plot(x_l, y_l, label = f'{x.name} vs {y.name} = {coef}*{x.name} + {const}')
+    plt.scatter(x, y, marker = 'x', c = 'g', label = f'{x.name} vs {y.name}')
+    plt.title('regresion lineal')
+    plt.xlabel(f'{x.name}')
+    plt.ylabel(f'{y.name}')
+    plt.show()
+
+regresion(y6, x2, x2_const)
+regresion(y7, x2, x2_const)
+print(df.groupby(['price']).count())
+#Se observa en las gráficas que la recta está descuadrada debido a los valores atípicos. Vamos a eliminar estos valores y a volverlo a intentar
+print(df['grade'].unique()) #El 12 y el 13 aparecen muy poco, son filas que podemos eliminar
+for i in range(len(df['grade'])):
+    if df['grade'][i] == 12 or df['grade'][i] == 13:
+        df.drop([i], axis = 0, inplace = True)
+    if df['price'][i] < 7000000:
+        pass
+    else:
+       df.drop([i], axis = 0, inplace = True)
+
+
+a = df['sqft_living15'].unique()
+
+
+print(df.groupby(['sqft_living15']).count()) #Como hay muchísimos valores únicos, vamos a agrupar por rangos
+'''for i in range(len(df['sqft_living15'])):
+    if int(df['sqft_living15'][i]) < 600:
+        df = df.drop([i], axis = 0)
+    if  600 <= int(df['sqft_living15'][i]) < 800:
+        df['sqft_living15'][i] = 700
+    if 800 <= int(df['sqft_living15'][i]) < 1000:
+        df['sqft_living15'][i] = 900
+    if 1000 <= int(df['sqft_living15'][i]) < 1500:
+        df['sqft_living15'][i] = 1250
+    if 1500 <= int(df['sqft_living15'][i]) < 2000:
+        df['sqft_living15'][i] = 1750
+    if 2000 <= int(df['sqft_living15'][i]) < 3000:
+        df['sqft_living15'][i] = 2500
+    if 3000 <= int(df['sqft_living15'][i]) < 4000:
+        df['sqft_living15'][i] = 3500
+    if 4000 <= int(df['sqft_living15'][i]) < 5000:
+        df['sqft_living15'][i] = 4500
+    if 5000 <= int(df['sqft_living15'][i]) < 6000:
+        df['sqft_living15'][i] = 5500
+    if 6000 <= int(df['sqft_living15'][i]):
+        df['sqft_living15'][i] = 6000
+'''
+x2 = df['price']
+
+y6 = df['grade']
+y7 = df['sqft_living15']
+
+x2_const = sm.add_constant(x2)
+regresion(y6, x2, x2_const)
+regresion(y7, x2, x2_const)
+
+
+'''y = df['price']
 x = df
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
@@ -104,7 +183,7 @@ plt.show()
 
 #Vamos a hacer gráficas para entender como funcionan los datos, con respecto al preio
 
-def puntos(xeje, yeje):
+'''def puntos(xeje, yeje):
     sns.scatterplot(data=df, x = xeje, y = yeje)
     plt.show()
 
@@ -113,43 +192,6 @@ puntos('price', 'condition')
 puntos('price', 'grade')
 puntos('price', 'sqft_living15')
 puntos('price', 'sqft_lot15')
-
+'''
 #Por último, construimos la recta de regresion lineal
 #La recta se construye con dos variables, por lo que hay que construir una para cada parcon price
-x2 = df['price']
-
-y2 = df['yr_built']
-y3 = df['floors']
-y5 = df['condition']
-y6 = df['grade']
-y7 = df['sqft_living15']
-y8 = df['sqft_lot15']
-
-x2_const = sm.add_constant(x2)
-
-def regresion(y, x, xconst):  
-    modelo = sm.OLS(y, xconst).fit()
-    pred = modelo.predict(xconst)
-    try:
-        const = modelo.params[0]
-        coef = modelo.params[1]
-        x_l = np.linspace(x.min(), x.max(), 50)
-        y_l = coef*x_l + const
-    except:
-        pass
-
-    plt.plot(x_l, y_l, label = f'{x.name} vs {y.name} = {coef}*{x.name} + {const}')
-    plt.scatter(x, y, marker = 'x', c = 'g', label = f'{x.name} vs {y.name}')
-    plt.title('regresion lineal')
-    plt.xlabel(f'{x.name}')
-    plt.ylabel(f'{y.name}')
-    plt.show()
-
-
-'''regresion(y2, x2, x2_const)
-#regresion(y3, x2, x2_const)
-#regresion(y4, x2, x2_const)
-regresion(y5, x2, x2_const)
-regresion(y6, x2, x2_const)
-regresion(y7, x2, x2_const)
-regresion(y8, x2, x2_const)'''
