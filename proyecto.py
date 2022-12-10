1209c84e5b898b66ba270580eabfff2928a451bc
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-10 12:14:32 +0100

    subelo

--- proyecto.py
+++ proyecto.py
@@ -3,3 +3,15 @@ import pandas as pd
 #Leemos con pandas el csv
 df = pd.read_csv('regression_data.csv', delimiter = ';')
 print(df.head())
+
+#Examinamos el csv
+print('COLUMNAS')
+print(df.columns)
+print('\n')
+print('INFO')
+df.info()
+
+#Observación: columnas no numércicas: date, bathrooms(arreglar), floors(arreglar), lat(arreglar), long(arreglar)
+# En principio no se ve ningún dato nulo  
+
+#Separamos variables numéricas y categóricas 
