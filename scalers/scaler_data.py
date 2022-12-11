41197b97a83a6edded6bab6720cc1f949b4cb0a8
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-11 02:05:06 +0100

    hghgh

--- scalers/scaler_data.py
+++ scalers/scaler_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
+import pylab as plt
+import pickle
 
 
 df1 = pd.read_csv('data/casas_limpio.csv', delimiter= ',')
@@ -9,7 +11,13 @@ df1 = pd.read_csv('data/casas_limpio.csv', delimiter= ',')
 #Transformamos los datos en una scale para poder pasarlos a pickle file
 ss = StandardScaler()
 df_transformado = ss.fit_transform(df1)
-#print(df_transformado)
+print(df_transformado)
 x_transformado = df_transformado[:, 0]
 y_transformado = df_transformado[:, 1]
 sns.scatterplot(x_transformado, y_transformado)
+plt.show()
+
+#Transformamos los datos a un fichero pickle
+with open('datos.pickle', 'wb') as f:
+    pickle.dump(df_transformado, f)
+
